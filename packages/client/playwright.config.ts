15b830b56eaf99ec194a25de7b5bf0f4094263f4
izzy <me@insrt.uk>
2026-01-21 18:01:31 +0000

    fix: use correct launch command for server

--- packages/client/playwright.config.ts
+++ packages/client/playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm run serve",
+    command: "mise start",
     url: "http://localhost:4173",
     reuseExistingServer: !process.env.CI,
   },
