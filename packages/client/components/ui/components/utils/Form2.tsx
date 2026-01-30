66f1b5654262eb4c272bcdba7e791c6d3503053f
Amy <amy+git@amogus.cloud>
2026-01-30 12:33:44 -0400

    chore: add Slider component to Form2
    Signed-off-by: Amy <amy+git@amogus.cloud>
    Signed-off-by: Amelia <amy+git@amogus.cloud>

--- packages/client/components/ui/components/utils/Form2.tsx
+++ packages/client/components/ui/components/utils/Form2.tsx
@@ -23,6 +23,7 @@ import {
   Text,
   TextField,
   typography,
+  Slider,
 } from "../design";
 import { TextEditor2 } from "../features/texteditor/TextEditor2";
 import { Column, Row } from "../layout";
@@ -412,6 +413,28 @@ function FormVirtualSelect<K, T>(props: {
 }
 
 /**
+ * Form slider
+ */
+const FormSlider = (
+  props: {
+    control: IFormControl<number>;
+  } & Omit<ComponentProps<typeof Slider>, "value">,
+) => {
+  const [local, remote] = splitProps(props, ["control"]);
+
+  return (
+    <Slider
+      {...remote}
+      value={local.control.value}
+      onInput={(event) => {
+        local.control.setValue(event.currentTarget.value);
+        local.control.markDirty(true);
+      }}
+    />
+  );
+};
+
+/**
  * Form reset button
  */
 const FormResetButton = (props: {
@@ -544,6 +567,7 @@ export const Form2 = {
   Radio: FormRadio,
   ButtonGroup: FormButtonGroup,
   VirtualSelect: FormVirtualSelect,
+  Slider: FormSlider,
   Reset: FormResetButton,
   Submit: FormSubmitButton,
   canSubmit,
