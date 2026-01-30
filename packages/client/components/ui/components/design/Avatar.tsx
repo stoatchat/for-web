edf296a582a7864c09f63a0b610d1cda99d67656
Amy <amy+git@amogus.cloud>
2026-01-30 12:07:43 -0400

    chore: shape takes preference over user preference
    Signed-off-by: Amy <amy+git@amogus.cloud>

--- packages/client/components/ui/components/design/Avatar.tsx
+++ packages/client/components/ui/components/design/Avatar.tsx
@@ -15,7 +15,7 @@ export type Props = {
   size?: number;
 
   /**
-   * Avatar shape
+   * Avatar shape override
    */
   shape?: "circle" | "square" | "rounded-square";
 
@@ -145,7 +145,11 @@ export function Avatar(props: Props) {
           class={css({ transition: "var(--transitions-fast) filter" })}
         >
           <Shape
-            style={{ "border-radius": `${state.theme.avatarRadius}%` }}
+            style={
+              !props.shape
+                ? { "border-radius": `${state.theme.avatarRadius}%` }
+                : {}
+            }
             shape={props.shape}
           >
             <Show when={props.interactive}>
