2b4fd52013e66effaba47802361059d186ff0a43
Amy <amy+git@amogus.cloud>
2026-01-30 12:00:06 -0400

    feat: add avatarRadius setting
    Signed-off-by: Amy <amy+git@amogus.cloud>

--- packages/client/components/ui/components/design/Avatar.tsx
+++ packages/client/components/ui/components/design/Avatar.tsx
@@ -5,6 +5,7 @@ import { styled } from "styled-system/jsx";
 
 import { Initials } from "../utils";
 
+import { useState } from "@revolt/state";
 import { Ripple } from "./Ripple";
 
 export type Props = {
@@ -116,6 +117,8 @@ const FallbackBase = styled("div", {
  * Partially inspired by Adw.Avatar API, we allow users to specify a fallback component (usually just text) to display in case the URL is invalid.
  */
 export function Avatar(props: Props) {
+  const state = useState();
+
   return (
     <ParentBase
       slot={props.slot}
@@ -141,7 +144,10 @@ export function Avatar(props: Props) {
           height="32"
           class={css({ transition: "var(--transitions-fast) filter" })}
         >
-          <Shape shape={props.shape}>
+          <Shape
+            style={{ "border-radius": `${state.theme.avatarRadius}%` }}
+            shape={props.shape}
+          >
             <Show when={props.interactive}>
               <Ripple />
             </Show>
