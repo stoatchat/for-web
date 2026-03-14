13f68f58c6354356202edaa65ad89ab53bfb351c
Jacob Schlecht <dadadah@echoha.us>
2026-03-14 16:48:45 -0600

    fix: Make the sync store only update if remote is different than local
    This commit was made without the use of generative AI.
    
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>

--- packages/client/components/state/stores/Sync.ts
+++ packages/client/components/state/stores/Sync.ts
@@ -1,3 +1,4 @@
+import isEqual from "lodash.isequal";
 import { batch } from "solid-js";
 
 import { ReactiveSet } from "@solid-primitives/set";
@@ -6,6 +7,8 @@ import { Client } from "stoat.js";
 import { State } from "..";
 
 import { AbstractStore } from ".";
+import { TypeNotificationOptions } from "./NotificationOptions";
+import { TypeOrdering } from "./Ordering";
 
 type SynchronisedStores = "ordering" | "notifications";
 
@@ -163,15 +166,22 @@ export class Sync extends AbstractStore<"sync", TypeSynchronisation> {
     if (import.meta.env.DEV)
       console.info(`[sync] merge ${key} at ${ts} with`, data);
 
+    const parsed = this.state[key].clean(JSON.parse(data));
     if (ts > this.ts(key)) {
       // if ts is newer, hydrate the store with it
-      const parsed = this.state[key].clean(JSON.parse(data));
       this.set("revision", key, ts);
       this.#blockSync.add(key);
       this.state.set(key, parsed);
     } else if (ts !== this.ts(key)) {
-      // if ts is old, trigger write to synchronise to remote
-      this.touch(key);
+      // if ts is old, trigger write to synchronise to remote, but only if the data has been updated
+      if (
+        !isEqual(
+          this.state[key].get(),
+          parsed as TypeOrdering & TypeNotificationOptions,
+        )
+      ) {
+        this.touch(key);
+      }
     }
   }
 
