dded489e70e651f60ec0b4d44d7fa9d2e1773b5d
Jed Blenkhorn <hello@jedbillyb.com>
2026-04-27 13:37:55 +1200

    fix: restore import group spacing in DeleteRole.tsx

--- packages/client/components/modal/modals/DeleteRole.tsx
+++ packages/client/components/modal/modals/DeleteRole.tsx
@@ -1,4 +1,5 @@
 import { Trans } from "@lingui-solid/solid/macro";
+
 import { useMutation } from "@tanstack/solid-query";
 import { Dialog, DialogProps } from "@revolt/ui";
 import { useModals } from "..";
