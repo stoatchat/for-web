a3801a9e87fc13c722d7bff35ee6f9fa4ae10805
Pecacheu <3608878+Pecacheu@users.noreply.github.com>
2026-05-19 22:44:24 -0400

    fix: Use innerWidth/Height instead of outer
    Signed-off-by: Pecacheu <3608878+Pecacheu@users.noreply.github.com>

--- packages/client/components/ui/components/features/voice/callCard/VoiceCallCard.tsx
+++ packages/client/components/ui/components/features/voice/callCard/VoiceCallCard.tsx
@@ -73,8 +73,8 @@ export function VoiceCallCardContext(props: { children: JSX.Element }) {
     if (e.pointerId !== pid) return;
     const sty = ref!.style,
       pos = ref!.getBoundingClientRect(),
-      left = e.clientX - ofsX + pos.width / 2 < outerWidth / 2,
-      top = e.clientY - ofsY + pos.height / 2 < outerHeight / 2;
+      left = e.clientX - ofsX + pos.width / 2 < innerWidth / 2,
+      top = e.clientY - ofsY + pos.height / 2 < innerHeight / 2;
 
     sty.transition = "all .2s cubic-bezier(0, 1.5, 0.85, 0.8)";
     setFloat(left ? (top ? "tl" : "bl") : top ? "tr" : "br");
