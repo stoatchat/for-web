7f5a5b8da784320e1808b1bd55caeb32704bf811
Pecacheu <3608878+Pecacheu@users.noreply.github.com>
2026-05-23 17:34:45 -0400

    feat: Device resize hooks
    Signed-off-by: Pecacheu <3608878+Pecacheu@users.noreply.github.com>

---
+++ packages/client/components/common/Device.tsx
@@ -0,0 +1,78 @@
+import {
+  createContext,
+  createSignal,
+  JSX,
+  onCleanup,
+  useContext,
+} from "solid-js";
+
+import { isMobileBrowser } from "@livekit/components-core";
+
+const style = getComputedStyle(document.body);
+
+export enum Layout {
+  DESKTOP = 1,
+  PHONE,
+  TABLET,
+}
+
+/** Device type and compatibility info */
+export class Device {
+  /** Max width for phone layout */
+  readonly phoneMaxWidth = style.getPropertyValue("--phone-max-width");
+
+  /** Max width for tablet layout */
+  readonly tabletMaxWidth = style.getPropertyValue("--tablet-max-width");
+
+  /** Layout type based on viewport size */
+  readonly layout;
+
+  /** Mobile device detection based on User Agent.
+
+   * **Warning:** Don't use unless absolutely necessary.
+   * Granular feature-detection is preferred when possible. */
+  readonly isMobile;
+
+  private pMedia;
+  private tMedia;
+  private setLayout;
+
+  constructor() {
+    this.isMobile = isMobileBrowser();
+
+    const [lo, setLo] = createSignal<Layout>(Layout.DESKTOP);
+    this.layout = lo;
+    this.setLayout = setLo;
+
+    this.pMedia = matchMedia(`(max-width: ${this.phoneMaxWidth})`);
+    this.tMedia = matchMedia(`(max-width: ${this.tabletMaxWidth})`);
+    (this.pMedia.onchange = this.tMedia.onchange = this.onLayout)();
+  }
+
+  onLayout() {
+    this.setLayout(
+      this.pMedia.matches
+        ? Layout.PHONE
+        : this.tMedia.matches
+          ? Layout.TABLET
+          : Layout.DESKTOP,
+    );
+  }
+
+  destroy() {
+    this.pMedia.onchange = this.tMedia.onchange = null;
+  }
+}
+
+const deviceCtx = createContext<Device>(null! as Device);
+
+/** Mount device context */
+export function DeviceContext(props: { children: JSX.Element }) {
+  const dev = new Device();
+  onCleanup(dev.destroy);
+
+  return <deviceCtx.Provider value={dev}>{props.children}</deviceCtx.Provider>;
+}
+
+/** Device type and compatibility info */
+export const useDevice = () => useContext(deviceCtx);
