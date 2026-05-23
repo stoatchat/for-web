ed2d55a5734df48542a6850ae32a1e8e84830bae
Pecacheu <3608878+Pecacheu@users.noreply.github.com>
2026-05-23 15:08:15 -0400

    fix: Use root for tsconfigRootDir instead of packages/client
    Signed-off-by: Pecacheu <3608878+Pecacheu@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,9 +16,11 @@ export default defineConfig([
       "**/dist/**",
       "**/styled-system/**",
     ],
+  },
+  {
     languageOptions: {
       parserOptions: {
-        tsconfigRootDir: import.meta.dirname + "/packages/client",
+        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
