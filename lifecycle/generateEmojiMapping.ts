0a979f05e7ae6e4e17f8b802769cd0fb15eb4088
Pecacheu <3608878+Pecacheu@users.noreply.github.com>
2026-05-25 19:00:25 -0400

    fix: Fix some TS errors & add more aliases to extensions JSON
    Signed-off-by: Pecacheu <3608878+Pecacheu@users.noreply.github.com>

--- lifecycle/generateEmojiMapping.ts
+++ lifecycle/generateEmojiMapping.ts
@@ -1,52 +1,53 @@
 import { resolve } from "jsr:@std/path";
 import emojiExtensions from "./emoji-extensions.json" with { type: "json" };
 
-const ordering = await fetch(
+const ordering = (await fetch(
   "https://raw.githubusercontent.com/googlefonts/emoji-metadata/main/emoji_17_0_ordering.json",
-).then((res) => res.json());
+).then((res) => res.json())) as typeof emojiExtensions.extensions;
 
 // Add our custom regional indicators
-for (const group of Object.keys(emojiExtensions.extensions)) {
-  const orderingGroup = ordering.find((og) => {
-    return og.group === emojiExtensions.extensions[group].group;
-  });
-
-  for (const emote of emojiExtensions.extensions[group].emoji) {
-    orderingGroup.emoji.push(emote);
-  }
+for (const group of emojiExtensions.extensions) {
+  const orderingGroup = ordering.find((og) => og.group === group.group)!;
+  for (const emote of group.emoji) orderingGroup.emoji.push(emote);
 }
 
 const Mapping: Record<string, string> = {};
 
 const RE_SHORTCODE = /^:[\w\-+]+:$/;
 
-for (const group of Object.keys(ordering)) {
-  for (const emote of ordering[group].emoji) {
+for (const group of ordering) {
+  for (const emote of group.emoji) {
     emote.base = (emote.base as number[]).filter(
       (codePoint) => codePoint !== 65039,
     );
 
     const emoji = String.fromCodePoint(...emote.base);
 
-    for (const shortcode of emote.shortcodes) {
-      if (!RE_SHORTCODE.test(shortcode)) continue;
+    for (const code of emote.shortcodes) {
+      if (!RE_SHORTCODE.test(code)) continue;
 
-      Mapping[shortcode.substring(1, shortcode.length - 1).toLowerCase()] =
-        emoji;
+      const name = code
+        .substring(1, code.length - 1)
+        .toLowerCase() as keyof typeof emojiExtensions.aliases;
+      Mapping[name] = emoji;
 
       // Check for aliases
-      const aliases =
-        emojiExtensions.aliases[
-          shortcode.substring(1, shortcode.length - 1).toLowerCase()
-        ];
+      const aliases = emojiExtensions.aliases[name];
       if (aliases) {
         for (const alias of aliases) {
-          if (!RE_SHORTCODE.test(":" + alias + ":")) continue;
+          if (!RE_SHORTCODE.test(`:${alias}:`)) continue;
           Mapping[alias.toLowerCase()] = emoji;
         }
+        delete emojiExtensions.aliases[name];
       }
     }
   }
+}
+
+//Test for unused aliases
+const unused = Object.keys(emojiExtensions.aliases);
+if (unused.length) {
+  throw "The following emoji aliases were not found: " + unused.join(", ");
 }
 
 Deno.writeTextFile(
