b6965f49f1bbe72003152d524b24b47b3c1c9acc
AsrayeDev <asrayeofficial@gmail.com>
2026-06-05 16:13:36 +1000

    chore: add GetChannelIcon and update existing
    Signed-off-by: AsrayeDev <asrayeofficial@gmail.com>

--- packages/client/components/markdown/plugins/anchors.tsx
+++ packages/client/components/markdown/plugins/anchors.tsx
@@ -10,6 +10,7 @@ import { useState } from "@revolt/state";
 import { Avatar, iconSize } from "@revolt/ui";
 import { Invite } from "@revolt/ui/components/features/messaging/elements/Invite";
 import { Symbol } from "@revolt/ui/components/utils/Symbol";
+import { getChannelIcon } from "@revolt/common";
 
 import MdChat from "@material-design-icons/svg/outlined/chat.svg?component-solid";
 import MdChevronRight from "@material-design-icons/svg/outlined/chevron_right.svg?component-solid";
@@ -130,7 +131,7 @@ export function RenderAnchor(
                 disabled={localProps.disabled}
                 href={internalUrl()}
               >
-                <Symbol>{channel()!.isVoice ? "headset_mic" : "tag"}</Symbol>
+                <Symbol>{getChannelIcon(channel(), "mention")}</Symbol>
                 {channel()!.name}
                 {params.exactMessage && (
                   <>
