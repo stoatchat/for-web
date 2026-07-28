91926fc143df2879069b4de0c12c7d86db7452b5
Jacob Schlecht <dadadah@echoha.us>
2026-07-27 19:47:12 -0600

    fix: Don't error out if one of your livekit nodes is down
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>

--- packages/client/components/rtc/state.tsx
+++ packages/client/components/rtc/state.tsx
@@ -232,7 +232,7 @@ class Voice {
     });
 
     // Gather latency
-    const selected = await Promise.race(
+    const selected = await Promise.any(
       this.getClient().configuration!.features.livekit.nodes.map(
         async (node) => {
           return fetch(node.public_url.replace("wss", "https")).then(() => {
