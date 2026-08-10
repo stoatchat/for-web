208d237e506143de48fb4c162ec43a81d42d92e3
İspik <ispik@ispik.dev>
2026-08-10 22:26:54 +0300

    feat: show in message metadata if a member is muted
    Signed-off-by: İspik <ispik@ispik.dev>

--- packages/client/components/app/interface/channels/text/Message.tsx
+++ packages/client/components/app/interface/channels/text/Message.tsx
@@ -1,13 +1,14 @@
 import {
   Accessor,
+  createContext,
+  createMemo,
+  createSignal,
   For,
   JSX,
   Match,
+  onMount,
   Show,
   Switch,
-  createContext,
-  createSignal,
-  onMount,
   useContext,
 } from "solid-js";
 
@@ -46,6 +47,7 @@ import {
 } from "../../../menus/UserContextMenu";
 
 import { EditMessage } from "./EditMessage";
+import { createIsTimedOut } from "@revolt/common/lib/createIsTimedOut";
 
 /**
  * Regex for matching URLs
@@ -143,6 +145,15 @@ export function Message(props: Props) {
   // Derive pronouns member takes precedence over author
   const pronouns = () =>
     props.message.member?.pronouns ?? props.message.author?.pronouns;
+
+  const timedOut = createIsTimedOut(() => props.message.member?.timeout);
+
+  const moderationPerms = createMemo(() =>
+    props.message.member?.server?.member?.hasPermission(
+      props.message.member!.server! ?? props.message.channel!,
+      "TimeoutMembers",
+    ),
+  );
 
   return (
     <MessageContext message={props.message} reactPicker={reactPicker}>
@@ -268,6 +279,16 @@ export function Message(props: Props) {
               <Tooltip content={t`Silent`} placement="top">
                 <Symbol size={16} fill>
                   notifications_off
+                </Symbol>
+              </Tooltip>
+            </Match>
+            <Match when={timedOut() && moderationPerms()}>
+              <Tooltip
+                content={t`Timed Out until ${props.message.member!.timeout!.toLocaleString()}`}
+                placement="top"
+              >
+                <Symbol size={16} fill>
+                  timer_off
                 </Symbol>
               </Tooltip>
             </Match>
