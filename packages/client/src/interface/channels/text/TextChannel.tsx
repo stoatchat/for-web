7377f82cbb042cc4b4dda44fafadaea1ca5e97d7
Jacob Schlecht <dadadah@echoha.us>
2026-08-10 17:53:06 -0600

    fix: strict not equals
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>

--- packages/client/src/interface/channels/text/TextChannel.tsx
+++ packages/client/src/interface/channels/text/TextChannel.tsx
@@ -183,7 +183,7 @@ export function TextChannel(props: ChannelPageProps) {
       (serverId, prevServerId) =>
         // This effect tracks channel, not serverId, therefore we must ensure the old serverId
         // is not the same as the current serverId
-        prevServerId != serverId &&
+        prevServerId !== serverId &&
         props.channel.type === "TextChannel" &&
         props.channel.server?.syncMembers(
           LARGE_SERVERS.includes(serverId) ? true : false,
