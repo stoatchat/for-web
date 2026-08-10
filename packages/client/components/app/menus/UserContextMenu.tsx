ecfb740bbb9884985971588d69c2d66b94cd9a17
Amelia <138383945+amycatgirl@users.noreply.github.com>
2026-08-10 11:25:01 -0400

    feat: show bot owner on profile cards when applicable (#1016)
    Signed-off-by: Amy <amy+git@amogus.cloud>
    Signed-off-by: Amelia <amy+git@amogus.cloud>

--- packages/client/components/app/menus/UserContextMenu.tsx
+++ packages/client/components/app/menus/UserContextMenu.tsx
@@ -1,13 +1,12 @@
 import { Trans } from "@lingui/solid/macro";
-import { useNavigate } from "@solidjs/router";
-import { type JSX, Match, Show, Switch } from "solid-js";
-import type { Channel, Message, ServerMember, User } from "stoat.js";
-
 import { useClient } from "@revolt/client";
 import { useModals } from "@revolt/modal";
 import { useSmartParams } from "@revolt/routing";
 import { useState } from "@revolt/state";
 import { Slider, Text } from "@revolt/ui";
+import { useNavigate } from "@solidjs/router";
+import { type JSX, Match, Show, Switch } from "solid-js";
+import type { Channel, Message, ServerMember, User } from "stoat.js";
 
 import MdAccountCircle from "@material-design-icons/svg/outlined/account_circle.svg?component-solid";
 import MdAddCircleOutline from "@material-design-icons/svg/outlined/add_circle_outline.svg?component-solid";
@@ -567,6 +566,7 @@ export function UserContextMenu(props: {
 export function floatingUserMenus(
   user: User,
   member?: ServerMember,
+  bot?: { owner: string },
   contextMessage?: Message,
   contextGroup?: Channel,
 ): JSX.Directives["floating"] & object {
@@ -574,6 +574,7 @@ export function floatingUserMenus(
     userCard: {
       user,
       member,
+      bot,
       // we could use message to display masquerade info in user card
     },
     /**
@@ -594,6 +595,11 @@ export function floatingUserMenus(
 
 export function floatingUserMenusFromMessage(message: Message) {
   return message.author
-    ? floatingUserMenus(message.author!, message.member, message)
+    ? floatingUserMenus(
+        message.author!,
+        message.member,
+        message.author!.bot,
+        message,
+      )
     : {}; // TODO: webhook menu
 }
