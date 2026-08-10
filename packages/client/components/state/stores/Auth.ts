99cbc161119a8292952e1e728b7361e53945772b
Pecacheu <3608878+Pecacheu@users.noreply.github.com>
2026-08-10 12:13:07 -0400

    refactor: Fix for rebase
    Signed-off-by: Pecacheu <3608878+Pecacheu@users.noreply.github.com>

--- packages/client/components/state/stores/Auth.ts
+++ packages/client/components/state/stores/Auth.ts
@@ -1,4 +1,4 @@
-import { useLingui } from "@lingui-solid/solid/macro";
+import { useLingui } from "@lingui/solid/macro";
 import { CONFIGURATION } from "@revolt/common";
 import { User } from "stoat.js";
 
