2a8b260f76c077c7be8a6374e98371abaa521131
AsrayeDev <asrayeofficial@gmail.com>
2026-08-12 15:13:30 -0400

    feat: Merge asraye/feat/voice-channel-mentions
    Signed-off-by: AsrayeDev <asrayeofficial@gmail.com>

--- packages/client/components/common/index.tsx
+++ packages/client/components/common/index.tsx
@@ -1,4 +1,5 @@
 export * from "./Device";
+export { getChannelIcon } from "./lib/channels";
 export { debounce } from "./lib/debounce";
 export { default as CONFIGURATION } from "./lib/env";
 export { insecureUniqueId } from "./lib/unique";
