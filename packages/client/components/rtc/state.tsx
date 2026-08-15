7a3d22d9828a36e3dafb4072cbdb42e4c2c60c81
Jacob Schlecht <Dadadah@users.noreply.github.com>
2026-08-15 16:24:17 -0600

    fix: Make device not go to sleep while in a call (#1498)
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>

--- packages/client/components/rtc/state.tsx
+++ packages/client/components/rtc/state.tsx
@@ -34,6 +34,7 @@ import {
 } from "@revolt/state/stores/Voice";
 import { VoiceCallCardContext } from "@revolt/ui/components/features/voice/callCard/VoiceCallCard";
 
+import { Device, useDevice } from "@revolt/common";
 import { InRoom } from "./components/InRoom";
 import { RoomAudioManager } from "./components/RoomAudioManager";
 import { VoiceProcessor } from "./VoiceProcessor";
@@ -85,6 +86,7 @@ class Voice {
   #setShowBar: Setter<boolean>;
 
   private sound: SoundController;
+  private device: Device;
 
   private openModal;
   private config;
@@ -96,9 +98,11 @@ class Voice {
     voiceSettings: VoiceSettings,
     modals: ModalController,
     sound: SoundController,
+    device: Device,
   ) {
     this.#settings = voiceSettings;
     this.sound = sound;
+    this.device = device;
 
     const [channel, setChannel] = createSignal<Channel>();
     this.channel = channel;
@@ -199,6 +203,8 @@ class Voice {
 
   async connect(channel: Channel, auth?: { url: string; token: string }) {
     this.disconnect();
+
+    this.device.setWakeLocked();
 
     const room = new Room({
       audioCaptureDefaults: {
@@ -312,6 +318,7 @@ class Voice {
   }
 
   disconnect() {
+    this.device.releaseWakeLock();
     try {
       const room = this.room();
       if (!room) return;
@@ -691,7 +698,8 @@ export function VoiceContext(props: { children: JSX.Element }) {
   const state = useState();
   const modals = useModals();
   const sound = useSound();
-  const voice = new Voice(state.voice, modals, sound);
+  const device = useDevice();
+  const voice = new Voice(state.voice, modals, sound, device);
 
   return (
     <voiceContext.Provider value={voice}>
