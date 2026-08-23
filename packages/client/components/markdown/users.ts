609274d4b37fba572eab7338391a5b7a09759ce9
Amelia <138383945+amycatgirl@users.noreply.github.com>
2026-08-23 18:39:55 -0400

    fix: unknown users when full member list is not loaded (#1529)
    Signed-off-by: Amelia <amy+git@amogus.cloud>

--- packages/client/components/markdown/users.ts
+++ packages/client/components/markdown/users.ts
@@ -1,4 +1,8 @@
-import { Accessor, createMemo } from "solid-js";
+import {
+  type Accessor,
+  type InitializedResource,
+  createResource,
+} from "solid-js";
 
 import { ServerMember, User } from "stoat.js";
 
@@ -53,6 +57,39 @@ export function userInformation(user?: User, member?: ServerMember) {
   };
 }
 
+async function getOrFetchUsers(options: {
+  ids: string[];
+  params: Accessor<{ serverId?: string }>;
+  filterNull?: boolean;
+}) {
+  const client = useClient();
+  const serverId = options.params().serverId;
+  return Promise.all(
+    options.ids.map(async (id) => {
+      const user =
+        client().users.get(id) ??
+        (await client()
+          .users.fetch(id)
+          .catch(() => null));
+
+      // Edge case: what if user fails to fetch on a spotty/intermittent conenction?
+      // Promise.all fails as soon as possible, so if one fetch fails, everything else
+      // does too and we don't get any data at all
+      if (!user) return undefined;
+
+      return userInformation(
+        user,
+        serverId
+          ? client().serverMembers.getByKey({
+              server: serverId,
+              user: user.id,
+            })
+          : undefined,
+      );
+    }),
+  ).then((arr) => (options.filterNull ? arr.filter((x) => x) : arr));
+}
+
 /**
  * Resolve multiple users by their ID within the current context
  * @param ids User IDs
@@ -62,33 +99,20 @@ export function userInformation(user?: User, member?: ServerMember) {
 export function useUsers(
   ids: string[] | Accessor<string[]>,
   filterNull?: boolean,
-): Accessor<(UserInformation | undefined)[]> {
-  const clientAccessor = useClient();
-
+): InitializedResource<(UserInformation | undefined)[]> {
   // TODO: use a context here for when we do multi view :)
   const params = useSmartParams();
+  const [users] = createResource(
+    {
+      ids: typeof ids === "function" ? ids() : ids,
+      params,
+      filterNull,
+    },
+    getOrFetchUsers,
+    { initialValue: [] as (UserInformation | undefined)[] },
+  );
 
-  // eslint-disable-next-line solid/reactivity
-  return createMemo(() => {
-    const client = clientAccessor()!;
-    const list = (typeof ids === "function" ? ids() : ids).map((id) => {
-      const user = client.users.get(id)!;
-
-      if (user) {
-        return userInformation(
-          user,
-          params().serverId
-            ? client.serverMembers.getByKey({
-                server: params().serverId!,
-                user: user.id,
-              })
-            : undefined,
-        );
-      }
-    });
-
-    return filterNull ? list.filter((x) => x) : list;
-  });
+  return users;
 }
 
 /**
